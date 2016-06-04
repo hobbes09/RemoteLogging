5997886f918f6e95d2843089d17e53e8f4ea86ef
hobbes09 <sourin.sutradhar@gmail.com>
2016-06-05 00:38:28 +0530

    views of log defined

--- log/views.py
+++ log/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from .models import Log
+from .serializers import LogSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+class LogList(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+    def get(self, request, format=None):
+        logs = Log.objects.all()
+        serializer = LogSerializer(logs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = LogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Log.objects.get(pk=pk)
+        except Log.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        log = self.get_object(pk)
+        serializer = LogSerializer(log)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        log = self.get_object(pk)
+        serializer = LogSerializer(log, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        log = self.get_object(pk)
+        log.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
