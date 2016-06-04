1435291fd88a9719aa30ad7672104f4df68da897
hobbes09 <sourin.sutradhar@gmail.com>
2016-06-04 23:17:10 +0530

    log admin interface customised

--- log/admin.py
+++ log/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Log
 
-# Register your models here.
-admin.site.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    fields = ['type', 'individual', 'body']
+    list_display = ['id', 'type', 'individual', 'body', 'created']
+    list_filter = ['type']
+    search_fields = ['id', 'type', 'individual', 'body']
+
+
+admin.site.register(Log, LogAdmin)
