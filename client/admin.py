88a334ce5c1e6f128428e71a24c6f5fe5a235e19
hobbes09 <sourin.sutradhar@gmail.com>
2016-06-04 23:15:53 +0530

    client admin interface customised

--- client/admin.py
+++ client/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from .models import Client
+from individual.models import Individual
 
-# Register your models here.
+class IndividualInline(admin.StackedInline):
+    model = Individual
+    extra = 2
 
-admin.site.register(Client)
+
+class ClientAdmin(admin.ModelAdmin):
+    fields = ['company_name', 'app_name', 'package_name']
+    list_display = ['id', 'slug', 'company_name', 'app_name', 'package_name', 'created_at', 'updated_at']
+    list_filter = ['company_name', 'app_name', 'package_name']
+    search_fields = ['id', 'slug', 'company_name', 'app_name', 'package_name']
+    inlines = [IndividualInline]
+
+admin.site.register(Client, ClientAdmin)
