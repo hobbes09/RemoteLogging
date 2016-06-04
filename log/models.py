647ee84124fcba12a53fd9a9010bc7c43570f78a
hobbes09 <sourin.sutradhar@gmail.com>
2016-06-04 23:17:37 +0530

    log model redefined -- added relationship with individual

--- log/models.py
+++ log/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from individual.models import Individual
 
 # Create your models here.
 
@@ -13,12 +14,15 @@ LOG_TYPES = (
 )
 
 class Log(models.Model):
-    id = models.CharField(max_length=100, primary_key=True, unique=True, default=uuid.uuid4)
+    id = models.CharField(max_length=100, primary_key=True, editable=False, unique=True, default=uuid.uuid4)
     created = models.DateTimeField(auto_now_add=True)
     body = models.TextField()
     type = models.CharField(choices=LOG_TYPES, default='V', max_length=100)
+    individual = models.ForeignKey(Individual, on_delete=models.CASCADE, verbose_name="individual", )
 
     class Meta:
         ordering = ('created',)
 
+    def __str__(self):
+        return (self.id.__str__() + "<->" + self.body.__str__())
 
