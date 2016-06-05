2021651b8c6367b633f372a3820779dadfdad774
hobbes09 <sourin.sutradhar@gmail.com>
2016-06-06 00:52:12 +0530

    disabling put request for log for now

--- log/views.py
+++ log/views.py
@@ -39,12 +39,13 @@ class LogDetail(APIView):
         return Response(serializer.data)
 
     def put(self, request, pk, format=None):
-        log = self.get_object(pk)
-        serializer = LogSerializer(log, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(data="Updating is disabled for Log for now", status=status.HTTP_400_BAD_REQUEST, content_type="text/html")
+        # log = self.get_object(pk)
+        # serializer = LogSerializer(log, data=request.data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response(serializer.data)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
         log = self.get_object(pk)
