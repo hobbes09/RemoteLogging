76bbf83cc0f7ee2f9091af6987da519a46958a13
hobbes09 <sourin.sutradhar@gmail.com>
2016-06-06 15:40:55 +0530

    added search of individuals in session

--- session/admin.py
+++ session/admin.py
@@ -3,9 +3,10 @@ from .models import Session
 
 
 class SessionAdmin(admin.ModelAdmin):
-    fields = ['individual', 'context', 'started_at', 'ended_at']
-    list_display = ['id', 'individual', 'context', 'started_at', 'ended_at', 'created_at', 'updated_at']
-    search_fields = ['id', 'individual__id', 'context']
+    fields = ['individual', 'context', 'status', 'started_at', 'ended_at']
+    list_display = ['id', 'individual', 'context', 'started_at', 'ended_at', 'status', 'created_at', 'updated_at']
+    list_filter = ['status']
+    search_fields = ['id', 'individual__id', 'individual__external_id', 'context', 'status']
 
 
 admin.site.register(Session, SessionAdmin)
