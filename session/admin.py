68b5cb69629073b9c898dc7d81bed033474b1fc6
hobbes09 <sourin.sutradhar@gmail.com>
2016-06-06 16:09:44 +0530

    session type info in admin

--- session/admin.py
+++ session/admin.py
@@ -3,10 +3,10 @@ from .models import Session
 
 
 class SessionAdmin(admin.ModelAdmin):
-    fields = ['individual', 'context', 'status', 'started_at', 'ended_at']
-    list_display = ['id', 'individual', 'context', 'started_at', 'ended_at', 'status', 'created_at', 'updated_at']
-    list_filter = ['status']
-    search_fields = ['id', 'individual__id', 'individual__external_id', 'context', 'status']
+    fields = ['individual', 'context', 'status', 'type', 'started_at', 'ended_at']
+    list_display = ['id', 'individual', 'context', 'type', 'started_at', 'ended_at', 'status', 'created_at', 'updated_at']
+    list_filter = ['status',  'type']
+    search_fields = ['id', 'individual__id', 'individual__external_id', 'context', 'status',  'type']
 
 
 admin.site.register(Session, SessionAdmin)
