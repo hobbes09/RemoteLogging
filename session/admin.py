d7916506fa329df60d184bb812219663f8f57644
hobbes09 <sourin.sutradhar@gmail.com>
2016-08-02 13:22:49 +0530

    added log list under session

--- session/admin.py
+++ session/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from .models import Session
+from log.models import Log
 
+class LogInline(admin.StackedInline):
+    model = Log
+    extra = 2
 
 class SessionAdmin(admin.ModelAdmin):
     fields = ['individual', 'context', 'status', 'type', 'started_at', 'ended_at']
     list_display = ['id', 'individual', 'context', 'type', 'started_at', 'ended_at', 'status', 'created_at', 'updated_at']
     list_filter = ['status',  'type']
     search_fields = ['id', 'individual__id', 'individual__external_id', 'context', 'status',  'type']
+    inlines = [LogInline]
 
 
 admin.site.register(Session, SessionAdmin)
