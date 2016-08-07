589d29d60ac8051eac8d3b768b26c5c688ccc008
hobbes09 <sourin.sutradhar@gmail.com>
2016-08-07 20:32:12 +0530

    removing inline resources in session and individual

--- individual/admin.py
+++ individual/admin.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 from .models import Individual
 from log.models import Log
 
-class LogInline(admin.StackedInline):
-    model = Log
-    extra = 2
+# class LogInline(admin.StackedInline):
+#     model = Log
+#     extra = 2
+#
+#
+# class IndividualAdmin(admin.ModelAdmin):
+#     fields = ['external_id', 'client', 'os', 'version']
+#     list_display = ['id', 'external_id', 'client', 'os', 'version', 'created_at', 'updated_at']
+#     list_filter = ['client', 'os', 'version']
+#     search_fields = ['id', 'external_id', 'client__slug', 'os', 'version']
+#     inlines = [LogInline]
+#
+# admin.site.register(Individual, IndividualAdmin)
 
 
 class IndividualAdmin(admin.ModelAdmin):
@@ -12,6 +22,5 @@ class IndividualAdmin(admin.ModelAdmin):
     list_display = ['id', 'external_id', 'client', 'os', 'version', 'created_at', 'updated_at']
     list_filter = ['client', 'os', 'version']
     search_fields = ['id', 'external_id', 'client__slug', 'os', 'version']
-    inlines = [LogInline]
 
 admin.site.register(Individual, IndividualAdmin)
